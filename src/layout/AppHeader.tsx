f20396e8dede80392b6ac24066d426dd9414dbfe
EngCuadrado <cuadradoengcs50@gmail.com>
2025-11-08 15:24:39 -0600

    fix: Update sidebar and header UI, add react-icons
    Replaced custom SVG icons in AppHeader with react-icons and added react-icons as a dependency. Refactored AppSidebar to improve menu structure, enable 'Others' section, and simplify expanded/collapsed logic. Updated SidebarWidget to display content instead of commented code. Adjusted AppLayout padding for improved layout.

--- src/layout/AppHeader.tsx
+++ src/layout/AppHeader.tsx
@@ -5,11 +5,13 @@ import { useSidebar } from "../context/SidebarContext";
 import { ThemeToggleButton } from "../components/common/ThemeToggleButton";
 import NotificationDropdown from "../components/header/NotificationDropdown";
 import UserDropdown from "../components/header/UserDropdown";
+import { CgMenuLeft } from "react-icons/cg";
+import { IoIosArrowBack } from "react-icons/io";
 
 const AppHeader: React.FC = () => {
   const [isApplicationMenuOpen, setApplicationMenuOpen] = useState(false);
 
-  const { isMobileOpen, toggleSidebar, toggleMobileSidebar } = useSidebar();
+  const { isMobileOpen, toggleSidebar, toggleMobileSidebar, isExpanded } = useSidebar();
 
   const handleToggle = () => {
     if (window.innerWidth >= 1024) {
@@ -49,6 +51,7 @@ const AppHeader: React.FC = () => {
             onClick={handleToggle}
             aria-label="Toggle Sidebar"
           >
+            {/* TODO: Mejorar vista mobile */}
             {isMobileOpen ? (
               <svg
                 width="24"
@@ -65,49 +68,51 @@ const AppHeader: React.FC = () => {
                 />
               </svg>
             ) : (
-              // TODO: Cambiar este ícono por un menú de hamburguesa que se transforme
-              <svg
-                width="16"
-                height="12"
-                viewBox="0 0 16 12"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  fillRule="evenodd"
-                  clipRule="evenodd"
-                  d="M0.583252 1C0.583252 0.585788 0.919038 0.25 1.33325 0.25H14.6666C15.0808 0.25 15.4166 0.585786 15.4166 1C15.4166 1.41421 15.0808 1.75 14.6666 1.75L1.33325 1.75C0.919038 1.75 0.583252 1.41422 0.583252 1ZM0.583252 11C0.583252 10.5858 0.919038 10.25 1.33325 10.25L14.6666 10.25C15.0808 10.25 15.4166 10.5858 15.4166 11C15.4166 11.4142 15.0808 11.75 14.6666 11.75L1.33325 11.75C0.919038 11.75 0.583252 11.4142 0.583252 11ZM1.33325 5.25C0.919038 5.25 0.583252 5.58579 0.583252 6C0.583252 6.41421 0.919038 6.75 1.33325 6.75L7.99992 6.75C8.41413 6.75 8.74992 6.41421 8.74992 6C8.74992 5.58579 8.41413 5.25 7.99992 5.25L1.33325 5.25Z"
-                  fill="currentColor"
-                />
-              </svg>
+              <span className="relative inline-flex items-center justify-center w-5 h-5">
+                <span
+                  className={`
+                    absolute transition-opacity duration-300 ease-in-out
+                    ${isExpanded ? "opacity-0" : "opacity-100"}
+                  `}
+                  aria-hidden={isExpanded}
+                >
+                  <CgMenuLeft className="size-5" />
+                </span>
+
+                <span
+                  className={`
+                  absolute transition-opacity duration-300 ease-in-out
+                  ${isExpanded ? "opacity-100" : "opacity-0"}
+                `}
+                  aria-hidden={!isExpanded}
+                >
+                  <IoIosArrowBack className="size-5" />
+                </span>
+              </span>
             )}
-            {/* Cross Icon */}
           </button>
 
-          <button
+          {/* <button
             onClick={toggleApplicationMenu}
             className="flex items-center justify-center w-10 h-10 text-gray-700 rounded-lg z-99999 hover:bg-gray-100 dark:text-gray-400 dark:hover:bg-gray-800 lg:hidden"
           >
-            <svg
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
+            <span
+              className={`relative inline-block w-5 h-5 transition-transform duration-300 ease-in-out [transform-style:preserve-3d] ${isExpanded  ? "[transform:rotateY(180deg)]" : ""
+                }`}
             >
-              <path
-                fillRule="evenodd"
-                clipRule="evenodd"
-                d="M5.99902 10.4951C6.82745 10.4951 7.49902 11.1667 7.49902 11.9951V12.0051C7.49902 12.8335 6.82745 13.5051 5.99902 13.5051C5.1706 13.5051 4.49902 12.8335 4.49902 12.0051V11.9951C4.49902 11.1667 5.1706 10.4951 5.99902 10.4951ZM17.999 10.4951C18.8275 10.4951 19.499 11.1667 19.499 11.9951V12.0051C19.499 12.8335 18.8275 13.5051 17.999 13.5051C17.1706 13.5051 16.499 12.8335 16.499 12.0051V11.9951C16.499 11.1667 17.1706 10.4951 17.999 10.4951ZM13.499 11.9951C13.499 11.1667 12.8275 10.4951 11.999 10.4951C11.1706 10.4951 10.499 11.1667 10.499 11.9951V12.0051C10.499 12.8335 11.1706 13.5051 11.999 13.5051C12.8275 13.5051 13.499 12.8335 13.499 12.0051V11.9951Z"
-                fill="currentColor"
-              />
-            </svg>
-          </button>
+              <span className="absolute top-0 left-0 inline-flex items-center justify-center w-full h-full [backface-visibility:hidden]">
+                <CgMenuLeft className="size-5" />
+              </span>
+
+              <span className="absolute top-0 left-0 inline-flex items-center justify-center w-full h-full [backface-visibility:hidden] [transform:rotateY(180deg)]">
+                <IoIosArrowBack className="size-5" />
+              </span>
+            </span>
+          </button> */}
         </div>
         <div
-          className={`${
-            isApplicationMenuOpen ? "flex" : "hidden"
-          } items-center justify-between w-full gap-4 px-5 py-4 lg:flex shadow-theme-md lg:justify-end lg:px-0 lg:shadow-none`}
+          className={`${isApplicationMenuOpen ? "flex" : "hidden"
+            } items-center justify-between w-full gap-4 px-5 py-4 lg:flex shadow-theme-md lg:justify-end lg:px-0 lg:shadow-none`}
         >
           <div className="flex items-center gap-2 2xsm:gap-3">
             {/* <!-- Dark Mode Toggler --> */}
