b55504613f4d2c946c47a54eb4439f782ae3eb97
engcuadrado <cuadradoeng@gmail.com>
2026-05-09 11:57:38 -0600

    cleaning

--- src/layout/AppSidebar.tsx
+++ src/layout/AppSidebar.tsx
@@ -63,16 +63,16 @@ const AppSidebar: React.FC = () => {
             name: "User Profile",
             path: "/profile",
         },**/
-        {
-            name: "Forms",
-            icon: <ListIcon />,
-            subItems: [{ name: "Form Elements", path: "/form-elements", pro: false }],
-        },
-        {
-            name: "Tables",
-            icon: <TableIcon />,
-            subItems: [{ name: "Basic Tables", path: "/basic-tables", pro: false }],
-        },
+        // {
+        //     name: "Forms",
+        //     icon: <ListIcon />,
+        //     subItems: [{ name: "Form Elements", path: "/form-elements", pro: false }],
+        // },
+        // {
+        //     name: "Tables",
+        //     icon: <TableIcon />,
+        //     subItems: [{ name: "Basic Tables", path: "/basic-tables", pro: false }],
+        // },
 /**        {
             name: "Pages",
             icon: <PageIcon />,
